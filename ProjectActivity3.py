f9d321c53486c9040898b27cf0ff11e9e0dd2d91
unknown <nicoleadrienne.vicente.iics@ust.edu.ph>
2021-10-10 18:13:19 +0800

    added proceed() function

--- ProjectActivity3.py
+++ ProjectActivity3.py
@@ -16,48 +16,90 @@ def intro():
     "Davao City", "Davao Oriental", "Davao del Sur", "General Santos City", "Sultan Kudarat", "Sarangani", "North Cotabato",
     "South Cotabato", "Agusan del Norte", "Agusan del Sur", "Surigao del Norte", "Dinagat Islands", "Cotabato City", "Lanao del Sur"]
 
-
-
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
     orig = input("Enter your Starting Location: ")
     dest = input("Enter your Destination: ")
 
     if orig in MECQ and dest in MECQ:
-        print("MECQ")
+        print("Hi, " , name , "! You are travelling between: MECQ areas.\n")
+        proceed(orig, dest)
     
     elif orig in GCQ_heightened and dest in GCQ_heightened:
-        print("GCQ heightened")
+        print("Hi, " , name , "! You are travelling between: Heightened GCQ areas.\n")
+        proceed(orig, dest)
 
     elif orig in GCQ_regular and dest in GCQ_regular:
-        print("GCQ regular")
+        print("Hi, " , name , "! You are travelling between: Regular GCQ areas.\n")
+        proceed(orig, dest)
     
     else:
         if age < 18:
-            print("interzonal < 18")
+            print("Hi, " , name , "! You are a MINOR travelling interzonally.\n")
+            proceed(orig, dest)
 
         elif age > 65:
             vax = input("Are you fully vaccinated? Y/N: ")
             if vax == "Y" or vax == "y":
-                print("interzonal > 65, vaxxed")
+                print("Hi, " , name , "! You are a FULLY VACCINATED ADULT travelling interzonally.\n")
+                proceed(orig, dest)
             elif vax == "N" or vax == "n":
-                print("interzonal > 65, non-vaxxed")
+                print(name, ", sadly, you are not allowed interzonal travel! Please follow the set restrictions.")
             else:
                 print("Wrong input!")
         
         else:
             a4 = input("Are you a frontliner? Y/N: ")
             if a4 == "Y" or a4 == "y":
-                print("frontliner")
+                print("Thank you for your service Frontliner ", name, "!")
+                proceed(orig, dest)
             elif a4 == "N" or a4 == "n":
                 pregnant = input("Are you pregnant? Y/N: ")
                 if pregnant == "Y" or pregnant == "y":
-                    print("pregnant")
+                    print("Woah! Congrats mama ", name, "! Safe travels and take care of your little one :)")
+                    proceed(orig, dest)
                 elif pregnant == "N" or pregnant == "n":
-                    print("CANNOT PASS")
+                    print(name, ", sadly, you are not allowed interzonal travel! Please follow the set restrictions.")
                 else:
                     print("Wrong input!")
             else:
                 print("Wrong input!")
+
+def proceed(orig, dest):
+    print("\n Here's what we have for you: \n")
+
+    main_api = "https://www.mapquestapi.com/directions/v2/route?"
+    key = "Rbx4dUy6MAKxomggVgx9psZAXsIRwpz6"
+
+    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+    json_data = requests.get(url).json()
+    print("URL: " + (url))
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+        print("=============================================")
+        print("Directions from " + (orig) + " to " + (dest))
+        print("Trip Duration: " + (json_data["route"]["formattedTime"]))
+        print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+        print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
+        print("=============================================")
+        for each in json_data["route"]["legs"][0]["maneuvers"]:
+            print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+        print("=============================================\n")
+    elif json_status == 402:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+        print("**********************************************\n")
+    elif json_status == 611:
+        print("**********************************************")
+        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+        print("**********************************************\n")
+    else:
+        print("************************************************************************")
+        print("For Staus Code: " + str(json_status) + "; Refer to:")
+        print("https://developer.mapquest.com/documentation/directions-api/status-codes")
+        print("************************************************************************\n")
+
 
 intro()
